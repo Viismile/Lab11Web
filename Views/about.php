e5fba9a37ef00a656b324789e6c879661f90e42a
Viismile <ciel3011@gmail.com>
2021-06-16 21:38:53 +0700

    Praktikum 12 Done

--- Views/about.php
+++ Views/about.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,25 +18,3 @@
     <?= $this->include('template/footer'); ?>
 </body>
 </html>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title><?= $title; ?></title>
-</head>
-<body>
-    <?= $this->include('template/header'); ?>
-    <section id="introduce">
-        <div class="row">
-            <img src="VV.jpg" title="Rofi Ismail" alt="Rofi Ismail" class="image-circle" width="230"
-            style="float: left; border: 2px solid black;">
-            <h1>Hello!</h1>
-            <p>Nama saya Rofi Ismail. Saya adalah seorang mahasiswa dari <i>Universitas Pelita Bangsa</i> yang saat ini sedang
-                mempelajari materi PHP Framework (Codeigniter) dalam mata kuliah <i>Pemrograman Web</i>.</p>
-        </div>
-    </section>
-    <?= $this->include('template/footer'); ?>
-</body>
-</html>
->>>>>>> 4c9f076516745c47dd29e4fa5b77886698d83cba
--- Views/contact.php
+++ Views/contact.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,32 +25,3 @@
     <?= $this->include('template/footer'); ?>
 </body>
 </html>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title><?= $title; ?></title>
-</head>
-<body>
-    <?= $this->include('template/header'); ?>
-    <section id="kontak">
-            <div class="login">
-                <input type="text" placeholder="Your Name" class="input">
-                <input type="text" placeholder="Your Email Address" class="input">
-            </div>
-
-            <div class="subject">
-                <input type="text" placeholder="Subject" class="input">
-            </div>
-
-            <div class="msg">
-                <textarea class="area" cols="35" rows="10" placeholder="Your Message" class="input"></textarea>
-            </div>
-
-            <button type="submit"> Send </button>
-        </section>
-    <?= $this->include('template/footer'); ?>
-</body>
-</html>
->>>>>>> 4c9f076516745c47dd29e4fa5b77886698d83cba
