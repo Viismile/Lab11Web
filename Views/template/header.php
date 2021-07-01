fcea988261e20075b09af08d8282757bf3afb7da
Rofi Ismail <56240078+Viismile@users.noreply.github.com>
2021-07-01 15:03:30 +0700

    Update praktikum 14 - done

--- Views/template/header.php
+++ Views/template/header.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <title><?= $title; ?></title>
     <link rel="stylesheet" href="<?= base_url('/style.css');?>">
+    <link rel="shortcut icon" href="<?= base_url('/R.png');?>">
 </head>
 <body>
     <div id="container">
     <div class="pembuat">Rofi Ismail</div>
     <header>
+        <img width="75" height="75" alt="" class="header-image" src="<?= base_url('/R.png');?>">
         <h1>Layout Sederhana</h1>
     </header>
     <nav>
